aa8ee05b19dfa8c7c6b9af49b886a89179536064
Jose Cerrato <josecerrato675@gmail.com>
2024-09-01 17:13:24 -0600

    first commit

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,68 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import { countries } from "../../data/countries";
+import styles from "./Form.module.css";
+import { SearchType } from "../../types";
+import Alert from "../Alert/Alert";
+
+type FormProps = {
+  fetchWeather: (search : SearchType) => Promise<void>
+}
+
+export default function Form({fetchWeather} : FormProps) {
+  const [search, setSearch] = useState<SearchType>({
+    city: "",
+    country: "",
+  });
+
+  const [alert, setAlert] = useState('')
+
+  const handleChange = (e : ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) =>{
+    setSearch({
+      ...search,
+      [e.target.name] : e.target.value
+    })
+  }
+
+  const handleSubmit = (e : FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if(Object.values(search).includes('')){
+      setAlert('Todos los campos son obligatorios')
+      return
+    }
+    setAlert('')
+    fetchWeather(search)
+
+  }
+
+  return (
+    <form className={styles.form} 
+      onSubmit={handleSubmit}
+    > 
+      {alert && <Alert>{alert}</Alert> }
+      <div className={styles.field}>
+        <label htmlFor="city">Ciudad: </label>
+        <input
+          type="text"
+          id="city"
+          name="city"
+          placeholder="Ciudad"
+          value={search.city}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="country">País: </label>
+        <select name="country" id="country" onChange={handleChange} value={search.country}>
+          <option value="">-- Seleccione un país --</option>
+          {countries.map((country) => (
+            <option key={country.code} value={country.code}>
+              {country.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <input type="submit" className={styles.submit} value="Consultar Clima" />
+    </form>
+  );
+}
